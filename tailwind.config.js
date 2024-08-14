5feb3a36873ae5e8044a387a40d13f9fdc59f9bd
dilan <dilanalbertmurcia@gmail.com>
2024-08-14 14:14:51 -0500

    Mejoró la legibilidad de los estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Onest Variable', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
